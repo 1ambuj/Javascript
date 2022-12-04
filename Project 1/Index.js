e598ea11ae01f21e9401522882a1cde7e06c50b9
1ambuj <ambuj83032@gmail.com>
2022-12-04 10:17:37 +0530

    creating variable in js

--- Project 1/Index.js
+++ Project 1/Index.js
@@ -1,1 +1,18 @@
-// nothing yet
+// creating variable in java script
+var a = 10;
+var b = 20;
+var c = 30;
+
+console.log(a+b+c)
+
+// creating variable using let key word
+
+let e = 10;
+let d = 20;
+
+console.log(e+d)
+
+//creating variable using const
+
+const name = "ambuj"
+console.log(name)
