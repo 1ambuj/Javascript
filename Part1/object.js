84199a57e9700201374f6822766e98daedbe1634
1ambuj <ambuj83032@gmail.com>
2023-01-28 08:42:38 +0530

    object destructuring and sperad operator

--- Part1/object.js
+++ Part1/object.js
@@ -7,7 +7,7 @@ const obj1 = {
     gender :"male"
 }
 // console.log(obj1.name)
-console.log(obj1["name"])
+//console.log(obj1["name"])
 // obj1.qualification  = "greguated"
 // console.log(obj1)
 //obj1["email add"] = "ambuj@1233"
@@ -16,4 +16,28 @@ console.log(obj1["name"])
 //take the key name from the variable
 let key = "email";
 obj1[key] = "ambuj@"
-console.log(obj1)
+// console.log(obj1)
+
+//object destructuring
+//let {name,age} = obj1
+const {name:nam,age:adge} = obj1
+
+// console.log(nam)
+// console.log(adge)
+
+//OBJECT INSIDE ARRAY NESTED DESTRUCTURING
+const personInfo = [
+    {name : "ambuj",age : 23,gender : "male"},
+    {name : "harshit",age : 23,gender : "male"},
+    {name : "shita",age : 23,gender : "female"}
+]
+let [person1, person2, person3] = personInfo
+// console.log(person1.gender)
+// console.log(person3.gender)
+personInfo.push({name:"anuj",age:20,gender:"male"})
+// console.log(personInfo)
+
+//object spred operator 
+const obj2 = {...obj1,name:"ambuj",age:22}
+console.log(obj2)
+
