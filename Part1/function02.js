60f09eb4d4a059c450761b4d82188ee5f69727ec
1ambuj <ambuj83032@gmail.com>
2023-01-31 23:19:48 +0530

    default and rest parameter in js

--- Part1/function02.js
+++ Part1/function02.js
@@ -59,4 +59,37 @@ if(true){
 }
 console.log(a)
 
-//
+//default parameter 
+
+function print_detail(a=20,b=6){//default parameter
+    return a + b
+}
+//const ans = print_detail()//genrate error
+
+console.log(print_detail(40,60))
+
+//REST PARAMETER
+function number(a,b,c){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+console.log(number(2,3,4,3,5,6,7))
+
+function number2(a,b,...c){//rest parameter
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+console.log(number2(2,3,4,3,5,6,7))
+
+//example add All ()
+function add_all(...number){
+    let count = 0
+    for(let num of number ){
+        count = count + num
+    }
+    return count
+}
+const total = add_all(1,3,3,4,5,6,6,7)
+console.log(total)
