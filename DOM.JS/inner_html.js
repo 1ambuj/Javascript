c85e4f10a0ee1e5e9d8b7b051314e0c00538bbe0
1ambuj <ambuj83032@gmail.com>
2023-02-15 21:29:05 +0530

    Adding element on the web page

--- DOM.JS/inner_html.js
+++ DOM.JS/inner_html.js
@@ -10,13 +10,44 @@ const element_class = document.querySelector(".to-do")
 // console.log(remove)
 
 //INEER HTML
-const inner = document.querySelector(".todo-list")
+//const inner = document.querySelector(".todo-list")
 
-inner.innerHTML += "<li>new todo</li>" //add element
-inner.innerHTML += "<li>new todo2</li>" 
-console.log(inner.innerHTML)
-
-//when we use inner-html and when we not use inner-html
+// inner.innerHTML += "<li>new todo</li>" //add element
+// inner.innerHTML += "<li>new todo2</li>" 
+// console.log(inner.innerHTML)
+ 
+// //when we use inner-html and when we not use inner-html
 //when we want to change the inner-html value then we use it
-//wen we want to add elemnt then we not use beacause it take more time
+//when we want to add elemnt then we not use beacause it take more time
+
+
+//document.creatElement()
+//append
+//prepand
+//remove
+
+//const todo_list = document.createElement("li")
+//const todo_text = document.createTextNode("my list item")
+//todo_list.textContent = "my list item"//it also add the content into li element
+//todo_list.append(todo_text)
+//onst todo_append_list =document.querySelector(".todo-list")
+//todo_append_list.append(todo_list)//add the li elemnt after the elemnet
+// todo_append_list.prepend(todo_list)//add the li elemnt bebore the existing element
+// console.log(todo_append_list)
+//console.log(todo_append_list)
+
+//remove()
+// const todo1 = document.querySelector(".todo-list li")
+// todo1.remove()
+// console.log(todo1)
+// console.log(todo_append_list)
+
+//before and after method
+
+const tag = document.createElement("li")
+tag.textContent = "my text"
+const take_element = document.querySelector(".todo-list")
+take_element.before(tag)
+take_element.after(tag)
+//console.log(take_element)
 
