ea46a20123923e5706056405f1df07754767aca2
1ambuj <ambuj83032@gmail.com>
2023-02-16 21:27:03 +0530

    Dom_cloning

---
+++ DOM.JS/Dom_clone.js
@@ -0,0 +1,8 @@
+//clone node 
+// const ul = document.querySelector(".todo-list")
+// const li = document.createElement("li")
+// li.textContent = "this is my content"
+// const li2 = li.cloneNode(true)
+// ul.append(li)
+// ul.prepend(li2)
+// console.log(ul)
