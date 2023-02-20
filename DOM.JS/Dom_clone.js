699244da5ed19dd108ac46c3daf50b647d807b16
1ambuj <ambuj83032@gmail.com>
2023-02-20 22:31:49 +0530

    dom clone methods

--- DOM.JS/Dom_clone.js
+++ DOM.JS/Dom_clone.js
@@ -6,3 +6,21 @@
 // ul.append(li)
 // ul.prepend(li2)
 // console.log(ul)
+
+//some of imprtant method of IE()
+//appendChild;
+//inserBfore;
+//replaceChild;
+//remove child
+
+const ul = document.querySelector(".todo-list")
+const li = document.createElement("li")
+li.textContent = "this is my content"
+//ul.appendChild(li)//use append child method
+
+//insertBefore method
+ul.insertBefore(li,ul)
+
+
+//replace child method
+
