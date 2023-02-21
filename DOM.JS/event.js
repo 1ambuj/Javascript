1136c7fc0924af3d086e357fb3eab662eb722d38
1ambuj <ambuj83032@gmail.com>
2023-02-21 20:38:35 +0530

    event methods

--- DOM.JS/event.js
+++ DOM.JS/event.js
@@ -1,3 +1,37 @@
 //when we perform any action called event
 //their are three types of event use in js
 
+// const btn = document.querySelector(".btn2")
+// console.dir(btn)//object representatoon of btn
+// btn.onclick = function(){//apply click event on btn class using onclick
+//     console.log("you click me!")
+// }
+
+//next method to add event in your web page
+// btn.addEventListener("click",function(){
+//     console.log("you click me!")
+//     console.log("value of this")
+//     console.log(this)
+// })
+
+//use arrow function
+
+// btn.addEventListener("click",()=>{
+//     console.log("you can click me")
+//     console.log("what's is your value")
+//     console.log(this)
+// }
+// )
+
+// le.log(list)const list = document.querySelectorAll(".list")
+// conso
+
+
+// //use event html file
+// const btn = document.querySelectorAll("button")
+// for(let button of btn){
+//     button.addEventListener("click",function(){
+//         console.log(this.textContent)
+//     })
+    
+// }
