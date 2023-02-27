ca951553d244013e734d9e91ba043f5f85dd70a8
1ambuj <ambuj83032@gmail.com>
2023-02-27 22:13:19 +0530

    practice in js

---
+++ DOM.JS/practice01.js
@@ -0,0 +1,10 @@
+//problem you can drive the car if your age lie between
+const age = +prompt("enter your age .")
+if(age>18){
+    console.log("you can drive the car!")
+}
+else{
+    console.log("you can not drive the car")
+}
+
+use 
