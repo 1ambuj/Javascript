45639063f45db84081203e053fb94cf03a927eb6
1ambuj <ambuj83032@gmail.com>
2023-03-13 22:52:00 +0530

    Asyncronous java script

---
+++ DOM.JS/Asyncronous_javascript.js/asyncronous.js
@@ -0,0 +1,18 @@
+//asyncronous javascript
+//Aj is a way of programing in which sequencence does not fix
+//syncronous programing
+// console.log("script begain")
+// for(i = 0 ;i<=10; i++){
+//     console.log(i)
+// }
+// console.log("script end!!")
+
+
+//asyncronous programing
+console.log("script start")
+setTimeout(()=>{
+    console.log("hello world! ")
+},1000)
+console.log("script end")
+
+//it is called asyncronous programing
