8833e539a450b2f248be78dad94f67ae3c162106
1ambuj <ambuj83032@gmail.com>
2023-03-14 19:53:10 +0530

    java script practice

--- DOM.JS/practice01.js
+++ DOM.JS/practice01.js
@@ -1,9 +1,44 @@
 //problem you can drive the car if your age lie between
-const age = +prompt("enter your age .")
-if(age>18){
-    console.log("you can drive the car!")
+// const age = +prompt("enter your age .")
+// if(age>18){
+//     console.log("you can drive the car!")
+// }
+// else{
+//     console.log("you can not drive the car")
+// }
+
+//print somthing using some method
+//using console.log
+//using alert method
+//using consol.write
+
+// console.log("hello world!!")
+// document.write("my website, hello")
+
+// //using alert method
+// alert("are you sure") 
+
+
+//program add to number using static number and dynamic number
+
+// const num1 = 4;
+// const num2 = 6;
+// //console.log(num1+num2)
+
+// //dynamic number
+
+// const a = +(prompt("enter first number !!"))
+// const b = +(prompt("enter second number!!"))
+// console.log(a+b)
+
+//note: parseInt method is used to convert string to number
+
+//second program
+//squre root finding
+const num = +(prompt("enter your number!! "))
+if(num/num==1){
+    console.log(num)
 }
 else{
-    console.log("you can not drive the car")
+    console.log(num*num)
 }
-
