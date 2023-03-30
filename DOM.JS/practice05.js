0f3d130a47f83640911d9cef6c6f5abe6593b793
1ambuj <ambuj83032@gmail.com>
2023-03-30 11:20:41 +0530

    practice of js

---
+++ DOM.JS/practice05.js
@@ -0,0 +1,34 @@
+//last digit same or not 
+
+// let num1 = prompt("enter the number : ")
+// let num2 = prompt("enter the second  number : ")
+// let num3 = prompt("enter the third  number : ")
+
+// let res = num1%10;
+// let res2 = num2%10;
+// let res3 = num3%10;
+
+// if((res)==(res2) && (res)==(res3)){
+//     console.log("The last digit of num1 ,num2 and num3 are the same")
+// }
+// else{
+//     console.log("the last digit number are not same")
+// }
+
+//palindrome string
+// let str = prompt("enter the string: ")
+// let reverse_str = ""
+
+// for(let i =str.length-1;i>=0;i--){
+//     reverse_str = reverse_str+str[i]
+    
+// }
+// if(str==reverse_str){
+//     console.log("The string is palindrome string! ")
+// }
+// else{
+//     console.log("not a palindrome string! ")
+// }
+//java script program to check the accurence of number
+
+
